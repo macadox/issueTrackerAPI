ec2b303489015add222f12b576fd1016ccb417a7
macadox-vsh <59931982+macadox@users.noreply.github.com>
2020-11-22 11:35:09 +0100

    added datasanitizers, helmet and rate limiting

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
 const path = require('path');
 const express = require('express');
+const helmet = require('helmet');
+const mongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
+const rateLimit = require('express-rate-limit');
 const morgan = require('morgan');
 const cookieParser = require('cookie-parser');
 
@@ -14,15 +18,26 @@ const viewsRouter = require('./routes/viewsRoutes');
 console.log(process.env.NODE_ENV);
 
 const app = express();
+app.use(helmet());
+
 app.set('view engine', 'pug');
 app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(morgan('dev'));
+const limiter = rateLimit({
+  max: 100,
+  windowMs: 60 * 1000,
+  message: 'Too many requests from this IP, please try again later.',
+});
+app.use('/api', limiter);
 
 app.use(express.json());
-// app.use(express.urlencoded({ extended: true, limit: '10kb' }));
 app.use(cookieParser());
+// app.use(express.urlencoded({ extended: true, limit: '10kb' }));
+
+app.use(mongoSanitize());
+app.use(xss());
 
 app.use((req, res, next) => {
   console.log('Hello from the middleware in express app');
