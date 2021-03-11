0aafad4919b8e33da2e675754b4e3adde45c957c
macadox-vsh <59931982+macadox@users.noreply.github.com>
2021-03-11 21:55:12 +0100

    added error further error handling

--- public/hooks/useFetch.js
+++ public/hooks/useFetch.js
@@ -1,16 +1,28 @@
 import { useState, useEffect } from 'react';
+import { useGlobalContext } from '../context';
 
 export const useFetch = (url) => {
   const [loading, setLoading] = useState(true);
   const [data, setData] = useState([]);
+  const { dispatchErrorAndRedirectTo } = useGlobalContext();
 
   useEffect(() => {
     const getData = async () => {
-      const response = await fetch(url);
-      const data = await response.json();
-
-      setData(data.data.data);
-      setLoading(false);
+      fetch(url)
+        .then((res) => res.json())
+        .then((data) => {
+          if (data.status === 'error' || data.status === 'fail') {
+            console.error(data.message);
+            dispatchErrorAndRedirectTo(data.message, '/error');
+          } else {
+            setData(data.data.data);
+            setLoading(false);
+          }
+        })
+        .catch((err) => {
+          console.error(err);
+          dispatchErrorAndRedirectTo(err, '/error');
+        });
     };
     getData();
   }, []);
